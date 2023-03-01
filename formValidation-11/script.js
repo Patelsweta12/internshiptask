0449d7a28ca8c3190dfa6586b82ca2a89dc1c678
Patelsweta12 <swetapatel11114@gmail.com>
2023-03-01 19:22:59 +0530

    post data

--- formValidation-11/script.js
+++ formValidation-11/script.js
@@ -12,9 +12,30 @@ submitButton.addEventListener("click", (e) => {
   let password = document.getElementById("pass").value;
 
   //   for FRIST NAME
+  // let takeFname = document.getElementById("fname");
+  // takeFname.addEventListener("keyup",()=>{
+  //   var alphNumericRegex = /^[0-9a-zA-Z]+$/;
+  //   if (firstName.trim() == "" || !firstName.match(alphNumericRegex)) {
+  //     document.getElementById("firstNameerror").innerHTML = "Enter your first name in alphanumeric form and no special character";
+  //     // let inputColor = document.getElementById("fname");
+  //     takeFname.style.border = "1px solid red";
+  //   }
+  //   else {
+  //     takeFname.style.border = "8px solid green";
+  //     // let inputColor = document.getElementById("fname");
+  
+  //     document.getElementById("firstNameerror").innerHTML = "";
+  //     console.log(firstName);
+  //   }
+    
+  // })
+  
+    
+
+
   var alphNumericRegex = /^[0-9a-zA-Z]+$/;
   if (firstName.trim() == "" || !firstName.match(alphNumericRegex)) {
-    document.getElementById("firstName-error").innerHTML = "Enter your first name in alphanumeric form and no special character";
+    document.getElementById("firstNameerror").innerHTML = "Enter your first name in alphanumeric form and no special character";
     let inputColor = document.getElementById("fname");
     inputColor.style.border = "1px solid red";
   }
@@ -22,13 +43,13 @@ submitButton.addEventListener("click", (e) => {
     let inputColor = document.getElementById("fname");
     inputColor.style.border = "1px solid green";
 
-    document.getElementById("firstName-error").innerHTML = "";
+    document.getElementById("firstNameerror").innerHTML = "";
     console.log(firstName);
   }
 
   //for LAST NAME
   if (lastName.trim() == "" || !lastName.match(alphNumericRegex)) {
-    document.getElementById("lastName-error").innerHTML =
+    document.getElementById("lastNameerror").innerHTML =
       "Enter your Last name in alphanumeric form and no special character";
     let inputColor = document.getElementById("lname");
     inputColor.style.border = "1px solid red";
@@ -37,7 +58,7 @@ submitButton.addEventListener("click", (e) => {
     let inputColor = document.getElementById("lname");
     inputColor.style.border = "1px solid green";
 
-    document.getElementById("lastName-error").innerHTML = "";
+    document.getElementById("lastNameerror").innerHTML = "";
     console.log(lastName);
   }
   //for PERSONAL email
@@ -47,7 +68,7 @@ submitButton.addEventListener("click", (e) => {
     personalEmail.trim() == "" ||
     !personalEmail.match(regexForPersonalEmail)
   ) {
-    document.getElementById("personalEmail-error").innerHTML = "Enter your valid  personal email";
+    document.getElementById("personalEmailerror").innerHTML = "Enter your valid  personal email";
     let inputColor = document.getElementById("pEmail");
     inputColor.style.border = "1px solid red";
   }
@@ -56,13 +77,13 @@ submitButton.addEventListener("click", (e) => {
     let inputColor = document.getElementById("pEmail");
     inputColor.style.border = "1px solid green";
 
-    document.getElementById("personalEmail-error").innerHTML = "";
+    document.getElementById("personalEmailerror").innerHTML = "";
     console.log(personalEmail);
   }
   //for ORG email
   var regexForOrgEmail = /^[a-zA-Z0-9._%+-]+@1rivet\.com$/;
   if (orgEmail.trim() == "" || !orgEmail.match(regexForOrgEmail)) {
-    document.getElementById("orgEmail-error").innerHTML = "Enter the valid Organization email ";
+    document.getElementById("orgEmailerror").innerHTML = "Enter the valid Organization email ";
     let inputColor = document.getElementById("oEmail");
     inputColor.style.border = "1px solid red";
   }
@@ -72,14 +93,14 @@ submitButton.addEventListener("click", (e) => {
     inputColor.style.border = "1px solid green";
 
 
-    document.getElementById("orgEmail-error").innerHTML = "";
+    document.getElementById("orgEmailerror").innerHTML = "";
     console.log(orgEmail);
     true;
   }
   // for PHONE NUMBER
   let phoneRegx = /^\d{10}$/;
   if (phoneNumber.trim() == "" || !phoneNumber.match(phoneRegx)) {
-    document.getElementById("phone-error").innerHTML = "Enter only 10 digit phone number";
+    document.getElementById("phoneerror").innerHTML = "Enter only 10 digit phone number";
     let inputColor = document.getElementById("phone");
     inputColor.style.border = "1px solid red";
   }
@@ -87,7 +108,7 @@ submitButton.addEventListener("click", (e) => {
 
     let inputColor = document.getElementById("phone");
     inputColor.style.border = "1px solid green";
-    document.getElementById("phone-error").innerHTML = "";
+    document.getElementById("phoneerror").innerHTML = "";
     console.log(phoneNumber);
     true;
   }
@@ -95,7 +116,7 @@ submitButton.addEventListener("click", (e) => {
   // for SALARY
   let salaryRegex = /^[0-9]*$/;
   if (salary <= 5000 || !salary.match(salaryRegex)) {
-    document.getElementById("salary-error").innerHTML = "Enter the valid Salary amount that is greater than 5000 ";
+    document.getElementById("salaryerror").innerHTML = "Enter the valid Salary amount that is greater than 5000 ";
     let inputColor = document.getElementById("salary");
     inputColor.style.border = "1px solid red";
   } else {
@@ -104,7 +125,7 @@ submitButton.addEventListener("click", (e) => {
     inputColor.style.border = "1px solid green";
 
 
-    document.getElementById("salary-error").innerHTML = "";
+    document.getElementById("salaryerror").innerHTML = "";
     console.log(salary);
     true;
   }
@@ -113,7 +134,7 @@ submitButton.addEventListener("click", (e) => {
   let passwordRegx =
     /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{10,16}$/;
   if (password.trim() == "" || !password.match(passwordRegx)) {
-    document.getElementById("password-error").innerHTML = "Enter the valid password";
+    document.getElementById("passworderror").innerHTML = "Enter the valid password";
     let inputColor = document.getElementById("pass");
     inputColor.style.border = "1px solid red";
   } else {
@@ -121,25 +142,29 @@ submitButton.addEventListener("click", (e) => {
 
     let inputColor = document.getElementById("pass");
     inputColor.style.border = "1px solid green";
-    document.getElementById("password-error").innerHTML = "";
+    document.getElementById("passworderror").innerHTML = "";
     console.log(salary);
     true;
   }
 
-  // // to post the data to jason file
-  // fetch(" http://localhost:3000/table", {
-  //   method: "POST",
-  //   body: JSON.stringify({
-  //     fname: firstName,
-  //     lname: lastName,
-  //     pEmail: personalEmail,
-  //     oEmail: orgEmail,
-  //     phone: phoneNumber,
-  //     salary: salary,
-  //     pass: password
-  //   }),
-  //   headers: { 'content-type': 'application/json' }
-  // }).then(response => response.json()).then(data => data).catch(error => error)
+  // to post the data to jason file
+  
+
+  if (firstNameerror.innerHTML=="" && lastNameerror.innerHTML == "" && personalEmailerror.innerHTML =="" && orgEmailerror.innerHTML=="" && phoneerror.innerHTML == "" && salaryerror.innerHTML =="" && passworderror.innerHTML == ""  ) {
+    fetch("http://localhost:3000/table", {
+    method: "POST",
+    body: JSON.stringify({
+      fname: firstName,
+      lname: lastName,
+      pEmail: personalEmail,
+      oEmail: orgEmail,
+      phone: phoneNumber,
+      salary: salary,
+      pass: password
+    }),
+    headers: { 'content-type': 'application/json' }
+  }).then(response => response.json()).then(data => data).catch(error => error)
+  }
 });
 
 
